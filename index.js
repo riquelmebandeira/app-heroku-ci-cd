f3f1adf51b06be850fba3badc30d3ed1d2a0bae6
Riquelme <riquelmebandeira1@outlook.com>
2022-02-10 15:38:27 -0300

    meu primeiro commit na plataforma heroku com Docker

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send(`<h1> Executando na porta ${port} </h1>`));
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Online: ${port}!`));
